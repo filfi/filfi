1e114b42207a703c7b61617220d060cf7c6ab4fd
leoton <15011116466@163.com>
2023-01-29 09:28:10 +0800

    init

---
+++ hardhat.config.ts
@@ -0,0 +1,34 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('hardhat-deploy');
+require('hardhat-deploy-ethers');
+require("dotenv").config()
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  defaultNetwork: "hyperspace",
+  networks: {
+    wallaby: {
+      url: "https://wallaby.node.glif.io/rpc/v0	",
+      accounts: [PRIVATE_KEY],
+    },
+    hyperspace: {
+      url: "https://api.hyperspace.node.glif.io/rpc/v1",
+      accounts: [PRIVATE_KEY]
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  // gasReporter: {
+  //   enabled: process.env.REPORT_GAS !== undefined,
+  //   currency: "USD"
+  // },
+  // etherscan: {
+  //   apiKey: process.env.ETHERSCAN_API_KEY,
+  // }
+};
